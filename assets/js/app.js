4b806d67ce3efa843ccdafaedf60c5556d9237c3
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2024-05-27 11:07:28 -0500

    Added Movies Data

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,15 @@
 
 // This app requires a server to handle import statements and CORS issues
 import * as utils from './utils.js';
+import movies from '../data/movies.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Organizer                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-utils.print('Welcome');
+const movieQuery = utils.select('.movie-query');
+const moviesAvailable = utils.select('.movies-available');
+const findMovies = utils.select('.find');
+const details = utils.select('.movie-details');
+
