905dc059b5a2b8cfae35a7b42df2a7b4536c1c59
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2024-05-28 13:28:21 -0500

    Added css styles and js funcionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*                                                       */
-/*  Andre Specht (2024)                                  */
-/*  github.com/mrspecht                                  */
+/*  Wellington Suero (2024)                              */
+/*  github.com/Wellfc                                    */
 /*                                                       */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -21,3 +21,107 @@ const moviesAvailable = utils.select('.movies-available');
 const findMovies = utils.select('.find');
 const details = utils.select('.movie-details');
 
+// Movie List
+const movieList = movies.map(movie => {
+    return {
+        title: movie.title,
+        year: movie.year,
+        duration: movie.runningTime,
+        plot: movie.description,
+        genre: movie.genre,
+        poster: movie.poster
+    }
+});
+
+// Movies Titles
+const movieTitles = movieList.map(movie => movie.title);
+
+//Show list of movies found
+function showMoviesFound(movies) {
+    const maxMoviesToShow = 5;
+    const moviesToDisplay = movies.slice(0, maxMoviesToShow);
+
+    moviesAvailable.innerHTML = '';
+    moviesAvailable.innerHTML = `
+        <ul class="movies-list">
+            ${moviesToDisplay.map(movie =>
+                `<li class="list-item">${movie}</li>`).join('')}
+        </ul>
+    `;
+
+    // Add event listeners to each list item
+    const listItems = utils.selectAll('.list-item');
+    listItems.forEach(item => {
+        utils.listen('click', item, () => {
+            movieQuery.value = item.textContent;
+            findMovies.click();
+            moviesAvailable.style.visibility = 'hidden';
+        });
+    });
+}
+
+//Show movie result
+function displayMovie(movie) {
+    details.innerHTML = '';
+    details.innerHTML = `
+        <div class="movie-poster">
+            <img src="${movie.poster}" alt="${movie.title}" class="movie-poster">
+        </div>
+        <div class="movie-info">
+            <h2 class="movie-title">${movie.title}</h2>
+            <div class="movie-data">
+                <span class="movie-year">${movie.year}</span>
+                <span class="separator"></span>
+                <span class="movie-duration">${movie.duration}</span>
+            </div>
+            <p class="movie-plot">${movie.plot}</p>
+            <div class="movie-genre">
+                ${movie.genre.map(genre => `<span class="genre">${genre}</span>`).join('')}
+            </div>
+        </div>
+    `;
+}
+
+// Find movie
+utils.listen('click', findMovies, () => {
+    const movie = movieList.find(movie => movie.title === movieQuery.value);
+    utils.print(movie);
+    if (movie) {
+        displayMovie(movie);
+        movieQuery.focus();
+    } else {
+        moviesAvailable.innerHTML = `
+        <ul class="movies-list">
+            <li class="not-found">Movie not found</li>
+        </ul>
+    `;
+    }
+});
+
+// Show movies available
+utils.listen('keyup', movieQuery, () => {
+    let query = movieQuery.value.toLowerCase().trim();
+
+    if (query.length > 2) {
+        const movies = movieTitles.filter(movie => movie.toLowerCase().includes(query));
+        if (movies.length) {
+            showMoviesFound(movies);
+            moviesAvailable.style.visibility = 'visible';
+        } else {
+            moviesAvailable.innerHTML = `
+                <ul class="movies-list">
+                    <li class="not-found">Movie not found</li>
+                </ul>
+            `;
+            moviesAvailable.style.visibility = 'visible';
+        }
+    } else {
+        moviesAvailable.style.visibility = 'hidden';
+    }
+});
+
+// Prevent form submission on button click
+const searchForm = utils.select('#search-form');
+utils.listen('submit', searchForm, event => {
+    event.preventDefault();
+});
